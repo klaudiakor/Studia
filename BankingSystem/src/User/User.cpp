bb6cb09979ed708f7ed7d11f8ec73b77f336036d
klaudiakor <83536013+klaudiakor@users.noreply.github.com>
2022-03-21 18:27:19 +0100

    Update User.cpp

--- BankingSystem/src/User/User.cpp
+++ BankingSystem/src/User/User.cpp
@@ -3,4 +3,3 @@
 User::User(
     const std::string& name, const std::string& surname, const std::string& log)
     : data{ .first_name{ name }, .last_name{ surname }, .login{ log } } {}
-// lista inicjalizujaca konstruktora
